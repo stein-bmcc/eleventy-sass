75dae9f87f3b2ad8f7ef2b75d64e950a102618d7
profstein <profstein@users.noreply.github.com>
2023-05-09 17:37:59 -0400

    add layout alias example

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/css/");
     eleventyConfig.addPassthroughCopy("./src/images/");
     eleventyConfig.addPassthroughCopy("./src/js/");
+    
+    eleventyConfig.addLayoutAlias('base', 'base.njk');
 
     // Return your Object options:
     return {
